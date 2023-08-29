a972329d3a6212e5bccdb906f4d38b38eecc0b5d
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-08-29 17:11:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-const React = require('react');
-const { Component } = require('react');
-require('./App.css');
 
+import React, { Component } from 'react';
+import './App.css';
 const Logo = () => {
   return (
-    React.createElement('div', null,
-      React.createElement('img', { src: 'src/logo.svg', alt: 'Logo' })
-    )
+    <div>
+      <img src="src/logo.svg" alt="Logo" />
+    </div>
   );
 };
+
+
 
 class App extends Component {
   render() {
     return (
-      React.createElement('div', { className: 'App' },
-        React.createElement('header', { className: 'App-header' },
-          React.createElement('img', { src: Logo, className: 'App-logo', alt: 'logo' }),
-          React.createElement('h1', { className: 'App-title' }, 'Welcome to React')
-        ),
-        React.createElement('p', { className: 'App-intro' },
-          "To get started, edit ",
-          React.createElement('code', null, 'src/App.js'),
-          " and save to reload."
-        )
-      )
+      <div className="App">
+        <header className="App-header">
+          <img src={Logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
 
-module.exports = {
-  App: App,
-  Logo: Logo
-};
+export default { App, Logo };
