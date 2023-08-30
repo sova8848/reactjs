ed15d5f1f1968d9b4ff2b7a312aebe71ab752788
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-08-30 15:28:37 +0530

    Update App.test.mjs

--- src/App.test.mjs
+++ src/App.test.mjs
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App.js';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(React.createElement(App, null), div);
+  ReactDOM.render(<App />, div);
 });
-
 
