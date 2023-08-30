41441f68d626d1d43119be3e132c5e6bb0955d32
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-08-30 15:10:30 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js', // Entry point of your application
+  entry: './src/index.js', // Replace with your entry file
   output: {
+    filename: 'bundle.js', // Output filename
     path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'bundle.js', // Output bundle filename
   },
   module: {
     rules: [
       {
-        test: /\.css$/, // Match .css files
-        use: ['style-loader', 'css-loader'], // Use these loaders
+        test: /\.css$/, // Match CSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
       },
     ],
   },
